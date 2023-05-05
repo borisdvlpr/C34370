b2e02fec15c722809878e187ebccba4acba2866d
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2023-05-05 14:21:27 +0200

    Update install.sh

--- SkyDive/install.sh
+++ SkyDive/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt -y install at
 curl -Lo - https://github.com/skydive-project/skydive-binaries/raw/jenkins-builds/skydive-latest.gz | gzip -d > skydive && chmod +x skydive && sudo mv skydive /usr/local/bin/
 cp SkydiveCtrl /usr/local/bin
 chmod +x /usr/local/bin/SkydiveCtrl
