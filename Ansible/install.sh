ecc7d3d37a5021533c01b8dbf518437cc520b029
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-23 21:02:34 +0100

    Update install.sh

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -6,8 +6,6 @@ then
    exit 0
 fi
 
-# Installing pip 
-apt-get -y install python3-pip
-
-# Installing Ansible
-python3 -m pip install ansible
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
