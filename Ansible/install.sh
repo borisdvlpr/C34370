e9ff845f69a9e06a1ef71e1f356702195cbdbd57
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-23 21:39:39 +0100

    Update install.sh

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $(id -u) -ne 0 ]
-then
-   echo "This script should be executed as root"
-   exit 0
-fi
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
 
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
