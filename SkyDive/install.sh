58118d5b9c6fe1c6a475bcf7933c18eda61f8e35
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-23 19:13:49 +0100

    Update install.sh

--- SkyDive/install.sh
+++ SkyDive/install.sh
@@ -7,8 +7,8 @@ then
    exit 0
 fi
 
-apt update
-apt -y install at zenity
+apt-get update
+apt-get -y install at zenity
 curl -Lo - https://github.com/skydive-project/skydive-binaries/raw/jenkins-builds/skydive-latest.gz | gzip -d > skydive && chmod +x skydive && sudo mv skydive /usr/local/bin/
 cp SkydiveCtrl /usr/local/bin
 chmod +x /usr/local/bin/SkydiveCtrl
