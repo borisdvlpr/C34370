046b3cfd256eedc4659189025a0055b77376bc14
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-23 19:12:57 +0100

    Update install.sh

--- Docker/install.sh
+++ Docker/install.sh
@@ -8,8 +8,8 @@ then
 fi
 
 apt update
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove $pkg; done
-apt install -y ca-certificates curl gnupg
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+apt-get install -y ca-certificates curl gnupg
 
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -17,8 +17,8 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Test
 sudo docker run hello-world
