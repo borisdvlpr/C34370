f2e572bb8ae03f35673e5e1b9fdb8d41738fda66
Lars Staalhagen <lars@staalhagen.dk>
2024-01-23 12:38:21 +0100

    '20240123-1238'

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check for root
+if [ $(id -u) -ne 0 ]
+then
+   echo "This script should be executed as root"
+   exit 0
+fi
+
+# Stop/disable some irrelevant services
+for p in avahi-daemon.service avahi-daemon.socket
+do
+   systemctl stop ${p}
+   systemctl mask ${p}
+done
+
+# Add user to vboxsf group for shared folder
+usermod -aG vboxsf user
