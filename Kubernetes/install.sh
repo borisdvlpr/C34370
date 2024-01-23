144baa04d2faf467d6afd9a3fbc37156c6fa0959
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-23 21:04:47 +0100

    Update install.sh

--- Kubernetes/install.sh
+++ Kubernetes/install.sh
@@ -10,12 +10,14 @@ fi
 # Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 dpkg -i minikube_latest_amd64.deb
+rm -f minikube_latest_amd64.deb
 
 # Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm -f kubectl kubectl.sha256
 
 # crictl
 VERSION="v1.29.0" # check latest version in /releases page at https://github.com/kubernetes-sigs/cri-tools/releases
@@ -27,13 +29,13 @@ rm -f crictl-$VERSION-linux-amd64.tar.gz
 # Check latest at https://github.com/Mirantis/cri-dockerd/releases
 wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.9/cri-dockerd_0.3.9.3-0.ubuntu-jammy_amd64.deb
 dpkg -i cri-dockerd_0.3.9.3-0.ubuntu-jammy_amd64.deb
+rm -f cri-dockerd_0.3.9.3-0.ubuntu-jammy_amd64.deb
 
 # container-networking
 # Check latest at https://github.com/containernetworking/plugins/releases
 CNI_PLUGIN_VERSION="v1.4.0"
 CNI_PLUGIN_TAR="cni-plugins-linux-amd64-$CNI_PLUGIN_VERSION.tgz" # change arch if not on amd64
 CNI_PLUGIN_INSTALL_DIR="/opt/cni/bin"
-
 curl -LO "https://github.com/containernetworking/plugins/releases/download/$CNI_PLUGIN_VERSION/$CNI_PLUGIN_TAR"
 sudo mkdir -p "$CNI_PLUGIN_INSTALL_DIR"
 sudo tar -xf "$CNI_PLUGIN_TAR" -C "$CNI_PLUGIN_INSTALL_DIR"
