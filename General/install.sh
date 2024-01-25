c69381e6bb277d4205a0072a9fe5b96f24d81f00
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 19:22:40 +0100

    Update install.sh

--- General/install.sh
+++ General/install.sh
@@ -5,11 +5,22 @@
 
 # Stop/disable some irrelevant services
 # NOTE: casper.service casper-md5check.service removed when not Linux Mint
-for p in avahi-daemon.service avahi-daemon.socket  cups.service cups-browsed.service wpa_supplicant.service ModemManager.service ufw.service
+for p in avahi-daemon.service avahi-daemon.socket cups.service cups-browsed.service wpa_supplicant.service ModemManager.service ufw.service
 do
    systemctl stop ${p}
    systemctl mask ${p}
 done
+if [ -f /etc/os-release ] 
+then
+   . /etc/os-release
+   if [ "${NAME}" = "Linux Mint" ]
+   then
+      systemctl stop casper.service
+      systemctl mask casper.service
+      systemctl stop casper-md5check.service
+      systemctl mask casper-md5check.service
+   fi
+fi
 
 # Add user to vboxsf group for shared folder
 usermod -aG vboxsf user
