818e645505eb3f7977f20560f30ac73eb822b925
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 15:17:17 +0100

    Update install.sh

--- SkyDive/install.sh
+++ SkyDive/install.sh
@@ -7,3 +7,6 @@ apt-get update
 apt-get -y install at zenity
 curl -Lo - https://github.com/skydive-project/skydive-binaries/raw/jenkins-builds/skydive-latest.gz | gzip -d > skydive && chmod +x skydive && sudo mv skydive /usr/local/bin/
 install -m 0755 skydivectl /usr/local/bin/skydivectl
+
+# Cleanup apt-get
+apt-get clean
