54ea19d4621f1c55e9d34df610fe02357faf76a8
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 15:17:04 +0100

    Update install.sh

--- OVS/install.sh
+++ OVS/install.sh
@@ -4,3 +4,7 @@
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
 
 apt-get -y install openvswitch-common openvswitch-switch openvswitch-switch-dpdk openvswitch-doc python3-openvswitch
+
+# Cleanup apt-get
+apt-get clean
+
