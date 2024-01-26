129ae4e9243dec42cb2fc02204236f23ef1583ba
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-26 11:52:31 +0100

    Update install.sh

--- Mininet/install.sh
+++ Mininet/install.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+
+# Get mininet
+git clone https://github.com/mininet/mininet
+
+# Install mininet
+mininet/util/install.sh
