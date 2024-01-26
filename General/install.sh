e9e6a553b6cae24b567c2003ac550503ac6a48a8
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-26 12:23:07 +0100

    Update install.sh

--- General/install.sh
+++ General/install.sh
@@ -23,7 +23,7 @@ then
 fi
 
 # Miscellaneous other packages
-apt-get -y install build-essential wireshark conntrack conntrackd xterm curl
+apt-get -y install build-essential wireshark conntrack conntrackd xterm curl net-tools ssh
 
 # Add user to sudoers file
 # grep -q -e "^user" /etc/sudoers || /usr/bin/echo -e "user\tALL = NOPASSWD: ALL" >> /etc/sudoers 
