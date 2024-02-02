87893cf865e01af36088b9eaed73ef33d8f6ca57
Lars Staalhagen <lars@staalhagen.dk>
2024-02-02 14:39:57 +0100

    '20240202-1439'

---
+++ 07-Ansible-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+
+# Cleanup apt-get
+apt-get clean
