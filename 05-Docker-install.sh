0df065dcc685f682d5dcd259599d762b97d5acb6
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-02-08 20:55:27 +0100

    Update 05-Docker-install.sh

--- 05-Docker-install.sh
+++ 05-Docker-install.sh
@@ -14,6 +14,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 
 # echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Change $UBUNTU_CODENAME to $VERSION_CODENAME if a real Ubuntu distribution is used
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
