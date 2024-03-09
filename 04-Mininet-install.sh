b67105c678d1b1fc2b1dbd887421307653b1d256
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-09 11:49:19 +0100

    Update 04-Mininet-install.sh

--- 04-Mininet-install.sh
+++ 04-Mininet-install.sh
@@ -2,6 +2,8 @@
 
 # Check for root
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+REALUSER=${SUDO_USER}
+[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
 # Get mininet
 git clone https://github.com/mininet/mininet
@@ -10,16 +12,16 @@ git clone https://github.com/mininet/mininet
 mininet/util/install.sh
 
 # Hack to get a better default font size in Xterm windows
-XRESOURCES=/home/user/.Xresources
+XRESOURCES=/home/${REALUSER}/.Xresources
 if [ -s $XRESOURCES ]; then
   grep -q -e "^xterm*faceName:"
   if [ $? -ne 0 ]; then
     echo "xterm*faceName: Monospace" >> $XRESOURCES
     echo "xterm*faceSize: 14" >> $XRESOURCES
-    sudo -u user "xrdb -merge $XRESOURCES
+    sudo -u ${REALUSER} "xrdb -merge $XRESOURCES"
   fi
 else
   echo "xterm*faceName: Monospace" > $XRESOURCES
   echo "xterm*faceSize: 14" >> $XRESOURCES
-  sudo -u user "xrdb -merge $XRESOURCES
+  sudo -u ${REALUSER} "xrdb -merge $XRESOURCES"
 fi
