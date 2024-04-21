4486ed5699817e3a09e92012bd6d966fcfc0ba95
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-04-21 12:37:09 +0200

    Update 04-Mininet-install.sh

--- 04-Mininet-install.sh
+++ 04-Mininet-install.sh
@@ -11,17 +11,4 @@ git clone https://github.com/mininet/mininet
 # Install mininet
 mininet/util/install.sh
 
-# Hack to get a better default font size in Xterm windows
-XRESOURCES=/home/${REALUSER}/.Xresources
-if [ -s $XRESOURCES ]; then
-  grep -q -e "^xterm*faceName:"
-  if [ $? -ne 0 ]; then
-    echo "xterm*faceName: Monospace" >> $XRESOURCES
-    echo "xterm*faceSize: 14" >> $XRESOURCES
-    sudo -u ${REALUSER} "xrdb -merge $XRESOURCES"
-  fi
-else
-  echo "xterm*faceName: Monospace" > $XRESOURCES
-  echo "xterm*faceSize: 14" >> $XRESOURCES
-  sudo -u ${REALUSER} "xrdb -merge $XRESOURCES"
-fi
+
